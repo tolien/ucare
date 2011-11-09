c02af1c11a1e6ad9caa7d746294f18015b4426ed
Michael Lees <michael.lees@strath.ac.uk>
2011-11-09 14:22:58 +0000

    Added graph interface

--- src/graphing/GraphTool.java
+++ src/graphing/GraphTool.java
@@ -1,5 +1,12 @@
 package graphing;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.swing.JPanel;
+
 public interface GraphTool {
 
+	public JPanel getGraph(int xSize, int ySize);
+	public void setRequestedData(ArrayList<String> labs, Date start, Date end);
 }
