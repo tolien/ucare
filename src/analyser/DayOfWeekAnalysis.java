3ccca3949929198c52fd0705a1cc043edd10e3fd
Stuart Swindells <stuart.swindells@strath.ac.uk>
2011-11-16 23:25:31 +0000

    Changes to print day names rather than numbers

--- src/analyser/DayOfWeekAnalysis.java
+++ src/analyser/DayOfWeekAnalysis.java
@@ -1,11 +1,16 @@
 package analyser;
 
+import java.text.DateFormatSymbols;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+
+import parser.Parser;
 
 public class DayOfWeekAnalysis extends Analyser implements OccupancyAnalyser
 {
+	private static DateFormatSymbols dfs = new DateFormatSymbols();
 	private static int INTERVAL = Calendar.DAY_OF_WEEK;
 	
 	@Override
@@ -24,6 +29,19 @@ public class DayOfWeekAnalysis extends Analyser implements OccupancyAnalyser
 	protected int getInterval()
 	{
 		return INTERVAL;
+	}
+	
+	public void getResult()
+	{
+		List<Integer> keys = Parser.asSortedList(data.keySet());
+		Iterator<Integer> it = keys.iterator();
+		
+		String[] days = dfs.getWeekdays();
+		while (it.hasNext())
+		{
+			int day = it.next();
+			System.out.println(days[day] + ": " + data.get(day));
+		}
 	}
 
 }
