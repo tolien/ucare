fc5a9a7cda01ed057446feea3ce79427008d1dcb
Stuart Swindells <stuart.swindells@strath.ac.uk>
2011-12-06 14:05:56 +0000

    Make changes to FileParser interface, make setting file to parse explicit through setFile rather than implicit constructor parameter

--- src/parser/FileParser.java
+++ src/parser/FileParser.java
@@ -1,9 +1,11 @@
 package parser;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Callable;
 
 public interface FileParser extends Callable<Occupancy>, Occupancy
 {
+	public void setFile(File f);
 	public void read() throws IOException;
 }
