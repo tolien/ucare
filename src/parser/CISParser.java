8ce7f2a4b03423ecbf1655fcfe2237accd1dd67b
Stuart Swindells <stuart.swindells@strath.ac.uk>
2011-12-06 14:22:11 +0000

    Add a dummy CISParser for now

---
+++ src/parser/CISParser.java
@@ -0,0 +1,54 @@
+package parser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class CISParser implements FileParser
+{
+
+	@Override
+	public Occupancy call() throws Exception
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getLabList()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Date, Integer> getAbsoluteOccupancy(String lab)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Date, Float> getRelativeOccupancy(String lab)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setFile(File f)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void read() throws IOException
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
