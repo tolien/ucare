cfc363e1a67d221b81e4a0877db27a01a7e21a77
Stuart Swindells <stuart.swindells@strath.ac.uk>
2012-04-06 13:55:28 +0100

    Use arguments to control the start time and run length, format the date so that it will copy into Excel

--- src/occupancy/PredictionDriver.java
+++ src/occupancy/PredictionDriver.java
@@ -1,4 +1,5 @@
 package occupancy;
+import java.text.DateFormat;
 import java.util.*;
 
 import analyser.*;
@@ -8,6 +9,7 @@ import parser.*;
 
 public class PredictionDriver
 {
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args)
 	{
 		if (args.length == 0)
@@ -15,6 +17,11 @@ public class PredictionDriver
 			System.exit(1);
 		} else
 		{
+			int year = Integer.parseInt(args[0]);
+			int month = Integer.parseInt(args[1]) - 1;
+			int day = Integer.parseInt(args[2]);
+			int days = Integer.parseInt(args[3]);
+			
 			DataSource parser = new Parser();
 			parser.read("data", new DSParserFactory());
 			
@@ -26,27 +33,24 @@ public class PredictionDriver
 			pred.occupancyData(data);
 			
 			Calendar c = Calendar.getInstance();
-			c.setTime(new Date(2012 - 1900, 2, 14, 0, 0, 0));
+			c.setTime(new Date(year - 1900, month, day, 0, 0, 0));
 
-			System.out.println(c.getTime());
 			List<Double> deviations = new ArrayList<Double>();
-			for (int i = 0; i < 24 * 14; i++)
+			for (int i = 0; i < 24 * days; i++)
 			{
-				c.add(Calendar.HOUR, 1);
 				Double predUsers = pred.getProbability(c.getTime()) * 106;
 				if (data.get(c.getTime()) != null)
 				{
 						Double actualUsers = data.get(c.getTime()) * 106;
-						System.out.println(c.getTime() + "\t" + predUsers.intValue() + "\t" + actualUsers.intValue());
+						System.out.println(DateFormat.getInstance().format(c.getTime()) + "\t" + predUsers.intValue() + "\t" + actualUsers.intValue());
 						deviations.add(predUsers - actualUsers);
 				}
 				else
 				{
-					System.out.println("No data for " + c.getTime());
+					//System.out.println("No data for " + c.getTime());
 				}
+				c.add(Calendar.HOUR, 1);
 			}
-			
-			System.out.println(c.getTime());
 			
 			System.out.println("Average deviation: " + Utility.average(deviations));
 			System.out.println("Median deviation: " + median(deviations));
